86d7f205a3e1c56a53cb112d7261ab33aeb87086
Michael Gross <mgstinger521@gmail.com>
2025-02-25 21:39:33 -0700

    Package Import Changes

--- MjWrapper/__init__.py
+++ MjWrapper/__init__.py
@@ -2,7 +2,8 @@ from .MjWrapper import MjWrapper
 
 __version__ = "0.1.0"
 __author__ = "Michael Gross"
+__all__ = ['MjWrapper'] # Expose only the MjWrapper class during `from MjWrapper import *`
 
-CAPTURE_PARAMETERS = ['time', 'qpos', 'qvel', 'qacc', 'xpos']
+CAPTURE_PARAMETERS = ['time', 'qpos', 'qvel', 'qacc', 'xpos'] # MjData fields to capture during simulation
 
 print(f"MjWrapper ({__version__}) is still under development. \n\rReport any issues to https://github.com/MGross21/MjWrapper/issues")
