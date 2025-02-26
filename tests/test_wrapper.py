d813a02a17cbcde677f6fed0195a903dae97cf33
Michael Gross <mgstinger521@gmail.com>
2025-02-26 16:16:46 -0700

    add unittest github automation

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -9,7 +9,7 @@ def test_wrapper():
     """Test 1: Create a simulation with a box and a leg, and run it with a sine controller."""
     model = "/tests/models/box_leg.xml"
     
-    sim = Wrapper(xml=model, duration=10, fps=30, resolution=(800, 600), controller=mjtb.sineController, amplitude=1, frequency=1)
+    sim = Wrapper(xml=model, duration=10, fps=30, resolution=(800, 600), controller=mjtb.sineController, amplitude=0.5, frequency=0.25)
     sim.runSim(render=True).renderMedia(codec="gif", title="sine_wave", save=True)
 
 if __name__ == "__main__":
