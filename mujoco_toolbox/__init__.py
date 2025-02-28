cf6f4828566d3a7b51cb7b252c74d3ca2028ee78
Michael Gross <mgstinger521@gmail.com>
2025-02-28 12:44:31 -0700

    Package Docstring Added

--- mujoco_toolbox/__init__.py
+++ mujoco_toolbox/__init__.py
@@ -1,3 +1,29 @@
+"""
+Mujoco Toolbox
+==============
+A toolbox for working with MuJoCo simulations. This package provides various utilities and controllers to facilitate
+the simulation process.
+
+Modules:
+--------
+- Wrapper: Contains the Wrapper class for interfacing with MuJoCo.
+- Controller: Includes sineController, cosineController, and randomController for controlling simulations.
+
+Constants:
+----------
+- CAPTURE_PARAMETERS: List of MjData fields to capture during simulation.
+
+Attributes:
+-----------
+- __version__ (str): The current version of the package.
+- __author__ (str): The author of the package.
+- __all__ (list): List of public objects of the module, as interpreted by `from module import *`.
+
+Notes:
+------
+This package is still under development. Report any issues to https://github.com/MGross21/mujoco-toolbox/issues.
+"""
+
 from .Wrapper import Wrapper
 from .Controller import sineController, cosineController, randomController
 import os
