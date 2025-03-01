4ae2c24a61d05a2c0b8b049655758d1c3d6f318c
Michael Gross <mgstinger521@gmail.com>
2025-03-01 16:34:02 -0700

    Update default capture parameters for simulation data

--- mujoco_toolbox/__init__.py
+++ mujoco_toolbox/__init__.py
@@ -32,7 +32,7 @@ __version__ = "0.0.1"
 __author__ = "Michael Gross"
 __all__ = ['Wrapper','Controller'] # Expose only the Wrapper class during `from Wrapper import *`
 
-CAPTURE_PARAMETERS = ['time', 'qpos', 'qvel', 'qacc', 'xpos'] # MjData fields to capture during simulation
+CAPTURE_PARAMETERS = ['time', 'qpos', 'qvel', 'act', 'qacc', 'xpos', 'xquat', 'xmat', 'ctrl', 'sensordata'] # MjData default fields to capture during simulation
 
 package_name = os.path.basename(os.path.dirname(os.path.dirname(__file__)))
 print(f"WARNING: {package_name} ({__version__}) is still under development. Report any issues to https://github.com/MGross21/{package_name}/issues")
