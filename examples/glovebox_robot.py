7bb94bf3881b96def88dd4327bf64559d923bbbb
Michael Gross <mgstinger521@gmail.com>
2025-03-20 04:31:08 -0700

    Add UR5 render images and update glovebox_robot example with joint position interpolation. (WIP)

--- examples/glovebox_robot.py
+++ examples/glovebox_robot.py
@@ -2,6 +2,7 @@ import mujoco_toolbox as mjtb
 from mujoco_toolbox import Wrapper, realTimeController
 import os
 import time
+import numpy as np
 
 # Load the model
 model_dir = os.path.abspath(os.path.join("..", "tests", "models", "UR5"))
@@ -9,13 +10,21 @@ urdf = os.path.join(model_dir, "ur5.urdf")
 meshes = os.path.join(model_dir, "meshes", "collision")
 
 
-ic = {
-    'qpos': [0, -1.57, 1.57, 0, 0, 0]
+initial = {
+    'qpos': [-0.707, -1.57, 1.57, -1.57, -1.57, 1.57]
 }
 
-with Wrapper(urdf, meshdir=meshes, initialConditions=ic, controller=realTimeController) as ur5:
+desired = {
+    "qpos": [0.707, -1, 1, -1.57, -1.57, 1.57]
+}
+
+# Generate a sequence of joint positions from initial to desired
+num_steps = 100
+qpos_sequence = np.linspace(initial["qpos"], desired["qpos"], num_steps)
+
+with Wrapper(urdf, meshdir=meshes, initialConditions=initial, controller=realTimeController) as ur5:
     ur5.liveView(show_menu=False) # Open the simulation window
     start_time = time.time()
     while time.time() - start_time < 10.0:
-        ur5._data.qpos[:] = ic["qpos"] # Rapidly Reassign the joint positions
+        ur5._data.qpos[:] = initial["qpos"] # Rapidly Reassign the joint positions
         
