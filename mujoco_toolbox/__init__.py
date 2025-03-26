515ea49d98ce9a050574a8dca314aea370f7e719
Michael Gross <mgstinger521@gmail.com>
2025-03-26 08:42:41 -0700

    Refactor imports in __init__.py and add PROGRESS_BAR constant

--- mujoco_toolbox/__init__.py
+++ mujoco_toolbox/__init__.py
@@ -29,7 +29,7 @@ from .controller import (
     sine_controller,
     step_controller,
 )
-from .utils import _Platform, _print_warning
+from .utils import _Platform
 from .wrapper import Wrapper
 
 __version__ = "0.3.0"
@@ -53,6 +53,7 @@ __all__ = [
 
 COMPUTER = _Platform()  # Singleton Instance
 MAX_GEOM_SCALAR: int = 2  # Scalar value for mujoco.Renderer.max_geom
+PROGRESS_BAR: bool = False  # Enable/Disable progress bar
 CAPTURE_PARAMETERS = [  # MjData default fields to capture during simulation
     "time",
     "qpos",
@@ -67,8 +68,10 @@ CAPTURE_PARAMETERS = [  # MjData default fields to capture during simulation
 ]
 
 if __version__.startswith("0"):
+    from .utils import _print_warning
     _print_warning(
         f"{__package__} (v{__version__}) is still under development.",
         f"Report any issues to https://github.com/MGross21/{__github_repo__}/issues",
     )
-del _Platform, _print_warning
+    del _print_warning
+del _Platform
