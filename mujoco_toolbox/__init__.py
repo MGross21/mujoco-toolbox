8263dcbc5aad976fea344a7b0fb855ace9a1fc6a
semantic-release <semantic-release>
2025-04-03 16:23:24 -0700

    chore(release): 0.5.0 [skip ci]

--- mujoco_toolbox/__init__.py
+++ mujoco_toolbox/__init__.py
@@ -39,7 +39,7 @@ from .controllers import (
 from .utils import _Platform
 from .wrapper import Wrapper
 
-__version__ = "0.4.5"
+__version__ = "0.5.0"
 __author__ = "Michael Gross"
 __github_repo__ = "mujoco-toolbox"
 __license__ = "MIT"
