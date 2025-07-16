71ea96c5cae8f98d22234275f64c77f0404a22f8
MGross21 <mgstinger521@gmail.com>
2025-07-16 00:14:05 -0700

    fix: streamline test functions for codec error handling and ensure GUI rendering requirement

--- tests/test_media.py
+++ tests/test_media.py
@@ -7,17 +7,16 @@ FILE_EXTENSIONS = ["mp4", "gif", "avi", "webm", "mov"]
 
 # Source: https://github.com/MGross21/mujoco-toolbox/issues/60
 MODEL = (Path(__file__).parent / "models/humanoid.xml").resolve().__str__()
+@pytest.mark.skipif(not mjtb.GUI_ENABLED, reason="This test requires GUI rendering to save media files.")
 def test_codec_error_mp4_issue_60():
-    sim = mjtb.Simulation(MODEL, fps=30, duration=3)
-    sim.run(render=mjtb.GUI_ENABLED).save("render.mp4")
+    mjtb.Simulation(MODEL, fps=30, duration=3).run(render=mjtb.GUI_ENABLED).save("render.mp4")
     assert os.path.exists("render.mp4"), "File render.mp4 was not created."
     os.remove("render.mp4")  # Clean up after test
 
 
-def test_all_codecs_save():
-    sim = mjtb.Simulation(MODEL, fps=5, duration=1, data_rate=20).run(render=mjtb.GUI_ENABLED)
-    for codec in FILE_EXTENSIONS:
-        sim.save(title=f"render.{codec}")
-
-        assert os.path.exists(f"render.{codec}"), f"File render.{codec} was not created."
-        os.remove(f"render.{codec}")  # Clean up after test
+@pytest.mark.skipif(not mjtb.GUI_ENABLED, reason="This test requires GUI rendering to save media files.")
+@pytest.mark.parametrize("codec", FILE_EXTENSIONS)
+def test_all_codecs_save(codec):
+    mjtb.Simulation(MODEL, fps=5, duration=1, data_rate=20).run(render=mjtb.GUI_ENABLED).save(title=f"render.{codec}")
+    assert os.path.exists(f"render.{codec}"), f"File render.{codec} was not created."
+    os.remove(f"render.{codec}")  # Clean up after test
