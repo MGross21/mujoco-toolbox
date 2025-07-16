8db79775a3006b4eb20f7cf9bcb6f67da8889ff4
MGross21 <mgstinger521@gmail.com>
2025-07-16 00:01:03 -0700

    Refactor dependencies in pyproject.toml and enhance media tests
    - Removed mediapy dependency and added ffmpeg as an extra for imageio.
    - Updated test_codec_error_mp4_issue_60 to check for file creation instead of raising an error.
    - Renamed and refactored test_codecs_dict_save to test_all_codecs_save for clarity and improved codec testing.
    - Added support for multiple video file extensions in the codec tests.

--- tests/test_media.py
+++ tests/test_media.py
@@ -3,17 +3,21 @@ from pathlib import Path
 import pytest
 import os
 
+FILE_EXTENSIONS = ["mp4", "gif", "avi", "webm", "mov"]
+
+# Source: https://github.com/MGross21/mujoco-toolbox/issues/60
 MODEL = (Path(__file__).parent / "models/humanoid.xml").resolve().__str__()
 def test_codec_error_mp4_issue_60():
     sim = mjtb.Simulation(MODEL, fps=30, duration=3)
-    with pytest.raises(ValueError):
-        sim.run(render=mjtb.GUI_ENABLED).save(codec="mp4")  # Source (https://github.com/mujoco/mujoco-toolbox/issues/60)
+    sim.run(render=mjtb.GUI_ENABLED).save("render.mp4")
+    assert os.path.exists("render.mp4"), "File render.mp4 was not created."
+    os.remove("render.mp4")  # Clean up after test
 
 
-def test_codecs_dict_save():
-    sim = mjtb.Simulation(MODEL, fps=5, duration=1, data_rate=20)
-    for codec in mjtb.sim._FFMPEG_CODEC_EXT.keys():
-        sim.run(render=mjtb.GUI_ENABLED).save(codec=codec)
+def test_all_codecs_save():
+    sim = mjtb.Simulation(MODEL, fps=5, duration=1, data_rate=20).run(render=mjtb.GUI_ENABLED)
+    for codec in FILE_EXTENSIONS:
+        sim.save(title=f"render.{codec}")
 
         assert os.path.exists(f"render.{codec}"), f"File render.{codec} was not created."
         os.remove(f"render.{codec}")  # Clean up after test
