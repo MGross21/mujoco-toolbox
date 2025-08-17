2a6a8b1676b6df086fecbd609d2b93299f8e92dc
MGross21 <mgstinger521@gmail.com>
2025-08-17 01:24:18 -0700

    Remove placeholder rs code. prep for conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 use pyo3::prelude::*;
 
-/// A simple function that adds two numbers
-#[pyfunction]
-fn add(a: i64, b: i64) -> PyResult<i64> {
-    Ok(a + b)
-}
 
 /// A Python module implemented in Rust
 #[pymodule]
---
+++ src/sim.rs
@@ -0,0 +1,6 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn run_sim() {
+    // Implementation goes here
+}
